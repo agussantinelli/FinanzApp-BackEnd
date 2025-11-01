904c97a0239e02833b338f3ccd640b4e298be52f
Agustin Santinelli <agustinsantinelli@gmail.com>
2025-11-01 05:01:06 -0300

    Implemento CORS

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -4,12 +4,30 @@ using WebAPI.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// HTTP y dependencias
 builder.Services.AddHttpClient();
 builder.Services.AddScoped<DolarApiClient>();
 builder.Services.AddScoped<DolarService>();
+
+// CORS - Permitir acceso desde tu frontend
+var corsPolicy = "_finanzappCors";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: corsPolicy, policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://localhost:3000",   
+                "https://localhost:3000"  
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -26,8 +44,10 @@ else
 
 app.UseHttpsRedirection();
 
+app.UseCors(corsPolicy);
+
 #region Endpoints
-app.MapDolarEndpoints(); 
+app.MapDolarEndpoints();
 #endregion
 
 app.Run();
